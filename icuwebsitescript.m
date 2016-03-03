3bddf21842e86a6b98ac12eb0bed151a506a31d6
Chitrakraj <chitrajcool@gmail.com>
2016-03-02 22:58:38 -0500

    Testing similarity score

--- icuwebsitescript.m
+++ icuwebsitescript.m
@@ -1,10 +1,10 @@
 %Background script for getting all the web pages from 4ICU
 disp('Save all the webpages locally');
-get4icuwebpages(false);
+%%get4icuwebpages(false);
 
 %% Process Webpages 
 disp('Process webpages saved');
 process4icuwebsite(false);
 
-disp('Exiting Normally');
-exit
+%%disp('Exiting Normally');
+%%exit
