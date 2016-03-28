4f73ce120c7523c12306a1d9c8c47a59b17441ed
Chitrakraj <chitrajcool@gmail.com>
2016-03-27 23:19:46 -0400

    Implemented to show only one record for exact match
    Improve the cost by not computing the score as soon as a perfect match
    is hit

--- calculateScore.m
+++ calculateScore.m
@@ -1,51 +1,55 @@
 function T = calculateScore(Ttest, Tactual)
-% %% EXAMPLE
-% %% d1 - Levenstein distance, d2 - Levenstein and editor distances
-% d1=strdist('statistics','mathematics')
-% d2=strdist('statistics','mathematics',2)
-% %%
-% %% |How to count substitutions:|
-% disp('Quantity of substitutions:')
-% disp(d2(2)-d2(1))
-% %%
-% %% |Case sensitive:|
-% strdist('Havelock North High School','Worcester Polytechnic Institute',2)
-% %%
-% %% |Case non-sensitive:|
-% strdist('Worcester Polytechnic Institute','Worcester Polytechnic Institute1',2,1)
-
-% act = {'Indian Institute of Technology Roorkee'};
-% tst = {'IIT Roorkee'};
-% 
-% 
-% for i = 1:length(act)
-%     for j = 1:length(tst)
-%         dd = strdist(act{i}, tst{j}, 2, 1);
-%         dd
-%     end
-% end
 
 %% Choose sample test
-[sample,idx] = datasample(Ttest(:,1),50);
-%Ttest(1:5,:)
-%z = Tactual(1:5,:);
-row = 1;
+[sample,idx] = datasample(Ttest(:,1),5);
+
 %% pre-allocate to the size of cartesian product
 rows = (length(sample)*height(Tactual));
-C = cell(rows,5);
+scoreC = cell(rows,5);
+
 for i = 1:length(sample)
-    for j = 1:height(Tactual) %Tactual
+    %% pre-allocate to the size of the actual set 
+    %% remove all those that does not fulfill the approx. match criteria
+    
+    subC = cell(height(Tactual),5); 
+    counter = 1;
+    for j = 1:height(Tactual) 
         actName = sample(i,1);
-        univName = Tactual(j,2); %Tactual
+        univName = Tactual(j,2); 
         univName = table2cell(univName);
         score = strdist(actName{1}, univName{1}, 2, 1);
+        substitutions = (score(2)-score(1));
+       
+        %% set threshold limit here
+        %% TODO - yet to determine a good threshold
+        if (score(1)<=10) %% COME UP WITH GOOD CONDITION
+            subC(counter,:) = {actName{1},univName{1}, score(1), score(2), substitutions};
+            counter = counter+1;
+        end
         
-        C(row,:) = {actName{1},univName{1}, score(1), score(2), (score(2)-score(1))};
-        row = row+1;     
+        
+        %% Approx. Match criteria
+        %% substitution cost = 0 and L-Score less than equal 2 are mostly exact matches
+        %% once a record hits an exact match, do not run the same record against others
+        if (score(1)<=2 && substitutions == 0)
+            subC(1:counter-1,:) = [];   
+            break
+        end        
     end
+    
+    %% if no match is found within the threshold defined,
+    %% enter the record in the score table with all scores = 100
+    %% filtering by 100 score can be used for evaluating accuracy and precision later
+    
+    
+    
+    to = find(~cellfun(@isempty,subC(:,1)));
+    from = find(cellfun(@isempty,scoreC(:,1)));    
+    scoreC(from(1):to(end)+from(1)-1,:) = subC(1:to(end),:);  
+    
 end
 
-T = cell2table(C,'VariableNames', {'AccountName', 'UniversityName', 'LScore', 'EScore', 'Substitutions'});
+T = cell2table(scoreC,'VariableNames', {'AccountName', 'UniversityName', 'LScore', 'EScore', 'Substitutions'});
 
 
 
