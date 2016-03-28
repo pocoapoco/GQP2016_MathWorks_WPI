2ed8413d64ecc30e8b57d6f2a943f92e349e4219
Chitrakraj <chitrajcool@gmail.com>
2016-03-28 01:13:29 -0400

    Implemented code to run a match with same alphabets

--- calculateScore.m
+++ calculateScore.m
@@ -1,7 +1,9 @@
 function T = calculateScore(Ttest, Tactual)
 
 %% Choose sample test
-[sample,idx] = datasample(Ttest(:,1),5);
+[sample,idx] = datasample(Ttest(:,1),50);
+
+%%sample = {'Gujarat Technological University'};
 
 %% pre-allocate to the size of cartesian product
 rows = (length(sample)*height(Tactual));
@@ -10,12 +12,15 @@ scoreC = cell(rows,5);
 for i = 1:length(sample)
     %% pre-allocate to the size of the actual set 
     %% remove all those that does not fulfill the approx. match criteria
-    
     subC = cell(height(Tactual),5); 
     counter = 1;
-    for j = 1:height(Tactual) 
-        actName = sample(i,1);
-        univName = Tactual(j,2); 
+    actName = sample(i,1);
+    
+    %% run sample only across the record that starts with the same alphabet 
+    Talphabet = Tactual(strncmpi(table2cell(Tactual(:,2)),actName{1}(1),1),:);
+    for j = 1:height(Talphabet) 
+        
+        univName = Talphabet(j,2); 
         univName = table2cell(univName);
         score = strdist(actName{1}, univName{1}, 2, 1);
         substitutions = (score(2)-score(1));
@@ -26,13 +31,14 @@ for i = 1:length(sample)
             subC(counter,:) = {actName{1},univName{1}, score(1), score(2), substitutions};
             counter = counter+1;
         end
-        
-        
+                
         %% Approx. Match criteria
         %% substitution cost = 0 and L-Score less than equal 2 are mostly exact matches
         %% once a record hits an exact match, do not run the same record against others
         if (score(1)<=2 && substitutions == 0)
-            subC(1:counter-1,:) = [];   
+            if counter>2
+                subC(1:counter-1,:) = [];   
+            end
             break
         end        
     end
@@ -40,8 +46,9 @@ for i = 1:length(sample)
     %% if no match is found within the threshold defined,
     %% enter the record in the score table with all scores = 100
     %% filtering by 100 score can be used for evaluating accuracy and precision later
-    
-    
+    if (isempty(find(~cellfun(@isempty,subC(1,1)), 1)))
+        subC(1,:) = {actName{1},' ', 100, 100, 100};
+    end
     
     to = find(~cellfun(@isempty,subC(:,1)));
     from = find(cellfun(@isempty,scoreC(:,1)));    
@@ -50,6 +57,10 @@ for i = 1:length(sample)
 end
 
 T = cell2table(scoreC,'VariableNames', {'AccountName', 'UniversityName', 'LScore', 'EScore', 'Substitutions'});
+
+%% delete empty rows to save space
+from = find(cellfun(@isempty,table2cell(T(:,1))), 1);
+T(from:end,:)=[];
 
 
 
