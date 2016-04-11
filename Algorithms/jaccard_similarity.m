f609fdf3ddf5a2234adaf531b4b3b9e22868fc85
Chitrakraj <chitrajcool@gmail.com>
2016-04-11 00:12:26 -0400

    Fixed bug

--- Algorithms/jaccard_similarity.m
+++ Algorithms/jaccard_similarity.m
@@ -15,38 +15,33 @@ elseif (n_gram == 2)
     z=regexp(word_User,'<s>|\w*|</s>','split');
     s2 = strcat(z(1:end-1),{' '},z(2:end));
 elseif (n_gram == 3)
-   words=regexp(word_Orig,'<s>|\w*|</s>','split'); %<s>|\w*|</s>
-   s1=cellfun(@(x,y,z) [x ' ' y ' ' z],words(1:end-2), words(2:end-1),words(3:end),'un',0);
-   words=regexp(word_User,'<s>|\w*|</s>','split'); %<s>|\w*|</s>
-   s2=cellfun(@(x,y,z) [x ' ' y ' ' z],words(1:end-2), words(2:end-1),words(3:end),'un',0);
+    words=regexp(word_Orig,'<s>|\w*|</s>','split'); %<s>|\w*|</s>
+    s1=cellfun(@(x,y,z) [x ' ' y ' ' z],words(1:end-2), words(2:end-1),words(3:end),'un',0);
+    words=regexp(word_User,'<s>|\w*|</s>','split'); %<s>|\w*|</s>
+    s2=cellfun(@(x,y,z) [x ' ' y ' ' z],words(1:end-2), words(2:end-1),words(3:end),'un',0);
 end
 
 uWords = union(s1,s2);
 
 % Binarize words
-S1 = zeros(size(fs));
+S1 = zeros(size(uWords));
 for k = 1:size(s1,1)
-    S1(k,:) = ismember(fs(k,:),s1);
+    S1(k,:) = ismember(uWords(k,:),s1);
 end
 
-S2 = zeros(size(fs));
+S2 = zeros(size(uWords));
 for k = 1:size(s2,1)
-    S2(k,:) = ismember(fs(k,:),s2);
+    S2(k,:) = ismember(uWords(k,:),s2);
 end
 
-% Find the intersection of the two strings
-inter_word = word_Orig & word_User;
+jaccardDist = pdist2(S1, S2, 'jaccard');
+jaccardIdx = 1 - jaccardDist;
 
-% Find the union of the two strings
-union_word = word_Orig | word_User;
-
-
-jaccardIdx = sum(inter_word(:))/sum(union_word(:));
-
-
-
-
-
-Index = jaccard_coefficient(S1,S2);
-Index
-
+% % Find the intersection of the two strings
+% inter_word = word_Orig & word_User;
+% 
+% % Find the union of the two strings
+% union_word = word_Orig | word_User;
+% 
+% jaccardIdx = sum(inter_word(:))/sum(union_word(:));
+% 
